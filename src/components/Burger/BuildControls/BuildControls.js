e6402509d5a545a3ffc81d88c705d14fd94ad21c
Neha Sadhvi <iam@nehasadhvi.com>
2018-11-20 08:35:29 -0800

    Added burger, ingredients and controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl'
+
+const buildControls = (props) => {
+
+    const controls = [
+        {label: 'Salad', type: 'salad'},
+        {label: 'Bacon', type: 'bacon'},
+        {label: 'Cheese', type: 'cheese'},
+        {label: 'Meat', type: 'meat'}
+    ];
+
+    return (
+        <div className={classes.BuildControls}>
+            {controls.map(control => {
+                return (
+                    <BuildControl key={control.label} label={control.label} />
+                );
+            })}
+        </div>
+    );
+};
+
+export default buildControls;
